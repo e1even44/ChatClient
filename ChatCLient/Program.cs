f751f507ad5c89cf7bd05e8a64ebc1669db929c6
Hilal Karsli <hilalkarsli@icloud.com>
2024-06-19 15:38:26 +0200

    init

---
+++ ChatCLient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class ChatClient
+{
+    private static TcpClient client;
+    private static NetworkStream stream;
+
+    static void Main(string[] args)
+    {
+        client = new TcpClient("172.30.62.42", 54321); // replace with server IP
+        stream = client.GetStream();
+
+        Thread readThread = new Thread(ReadMessages);
+        readThread.Start();
+
+        while (true)
+        {
+            string message = Console.ReadLine();
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+    }
+
+    private static void ReadMessages()
+    {
+        byte[] buffer = new byte[1024];
+        int byteCount;
+
+        while ((byteCount = stream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            string message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            Console.WriteLine("Server: " + message);
+        }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class ChatServer
+{
+    private static TcpListener listener;
+
+    static void Main(string[] args)
+    {
+        listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
+        }
+    }
+
+    private static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int byteCount;
+
+        while ((byteCount = stream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            string message = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            Console.WriteLine("Received: " + message);
+            byte[] response = Encoding.UTF8.GetBytes("Server received: " + message);
+            stream.Write(response, 0, response.Length);
+        }
+    }
+}
